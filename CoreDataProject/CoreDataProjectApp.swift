17f3f84fa32a613dfa59c9c6567f2b5f8bc82d5d
Takasur Azeem <takasurazeem@gmail.com>
2022-05-10 17:31:31 +0500

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Takasur Azeem on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
