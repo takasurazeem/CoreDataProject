7669cda0489f8d03568f4985de4bda79c1a5aeb5
Takasur Azeem <takasurazeem@gmail.com>
2022-05-10 18:36:13 +0500

    initial project setup to begin with.
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Takasur Azeem on 10/05/2022.
+//
+
+import CoreData
+
+class DataController: ObservableObject {
+    
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
